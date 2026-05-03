e11aab35ae655f7c40bb4fde7b21dd734bf7b8a7
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2026-05-04 00:11:43 +0500

    Refactor FeaturedProducts component data query
    Refactor FeaturedProducts component to simplify data handling and improve error messaging.

--- src/components/features/home/FeaturedProducts/index.tsx
+++ src/components/features/home/FeaturedProducts/index.tsx
@@ -1,31 +1,24 @@
 "use client"
 import Card from "@/components/ui/Card";
 import { useQuery } from "@tanstack/react-query";
-import { productService } from "@/services/product.service";
-import { ProductResponse } from "@/services/product.service";
-
-type ProductsApiResponse = {
-  success: boolean;
-  products: ProductResponse[];
-  message: string;
-};
+import { productService, ProductResponse } from "@/services/product.service";
 
 export default function FeaturedProducts() {
-  const { data, isLoading, isError } = useQuery<ProductsApiResponse>({
+  const { data, isLoading, isError } = useQuery<ProductResponse[]>({
     queryKey: ["products"],
     queryFn: () => productService.getProduct(), 
   });
 
   if (isLoading) return <p>Loading...</p>;
-  if (isError) return <p>Something went wrong.</p>;
+  if (isError) return <p>Something went wrong</p>;
 
   return (
     <div className="space-y-5 flex flex-col container">
       <h2 className="text-xl md:text-3xl font-semibold">Featured Products</h2>
       <div className="grid space-x-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-5">
-        {data?.products?.map((product: ProductResponse, index: number) => (
+        {data?.map((product: ProductResponse) => (
           <Card
-            key={product._id} 
+            key={product._id}
             name={product.name}
             thumbnail={product?.thumbnail}
             description={product.description}
