0e9051abeabc70f71b8bed05ba035f0592d6444a
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2026-05-03 23:44:31 +0500

    Fix mapping of products in FeaturedProducts component

--- src/components/features/home/FeaturedProducts/index.tsx
+++ src/components/features/home/FeaturedProducts/index.tsx
@@ -23,7 +23,7 @@ queryFn: async () => {
 
        <div className="grid space-x-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-5">
    
-{data?.products?.map((product:ProductResponse, index:number) => (
+{data?.products?.products?.map((product:ProductResponse, index:number) => (
   <Card
     key={index}
     name={product.name}
