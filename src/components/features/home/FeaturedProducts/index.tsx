371d57d86d0b9a232f86037cbdbc02d884728320
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2026-05-04 00:06:43 +0500

    Refactor FeaturedProducts component for clarity

--- src/components/features/home/FeaturedProducts/index.tsx
+++ src/components/features/home/FeaturedProducts/index.tsx
@@ -1,38 +1,39 @@
 "use client"
-
 import Card from "@/components/ui/Card";
- import { useQuery } from "@tanstack/react-query";
- import { productService } from "@/services/product.service";
+import { useQuery } from "@tanstack/react-query";
+import { productService } from "@/services/product.service";
 import { ProductResponse } from "@/services/product.service";
-export default function FeaturedProducts(){
-   type ProductsApiResponse = {
+
+type ProductsApiResponse = {
+  success: boolean;
   products: ProductResponse[];
+  message: string;
 };
-const { data, isLoading, isError } = useQuery<ProductsApiResponse>({
+
+export default function FeaturedProducts() {
+  const { data, isLoading, isError } = useQuery<ProductsApiResponse>({
     queryKey: ["products"],
-queryFn: async () => {
-    const products = await productService.getProduct();
-    return { products };
-  },  });
+    queryFn: () => productService.getProduct(), 
+  });
 
- console.log(data,'product')
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Something went wrong.</p>;
 
-    return(
-        <div className="space-y-5 flex flex-col container">
-            <h2 className="text-xl md:text-3xl font-semibold">Featured Products</h2>
-
-       <div className="grid space-x-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-5">
-   
-{data?.products?.map((product:ProductResponse, index:number) => (
-  <Card
-    key={index}
-    name={product.name}
-    thumbnail={product?.thumbnail}
-    description={product.description}
-    price={product.price}
-    stock={product.stock}
-  />
-))}      </div>
-        </div>
-    )
+  return (
+    <div className="space-y-5 flex flex-col container">
+      <h2 className="text-xl md:text-3xl font-semibold">Featured Products</h2>
+      <div className="grid space-x-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-5">
+        {data?.products?.map((product: ProductResponse, index: number) => (
+          <Card
+            key={product._id} 
+            name={product.name}
+            thumbnail={product?.thumbnail}
+            description={product.description}
+            price={product.price}
+            stock={product.stock}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
